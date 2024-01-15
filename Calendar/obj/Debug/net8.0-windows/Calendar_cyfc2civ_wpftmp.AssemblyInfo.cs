8019fe32c6760566bf09fd30d787332f2e17ad6a
Hugo Teixeira <hugo127c@gmail.com>
2024-01-15 19:12:23 +0000

    Add icon

--- Calendar/obj/Debug/net8.0-windows/Calendar_cyfc2civ_wpftmp.AssemblyInfo.cs
+++ Calendar/obj/Debug/net8.0-windows/Calendar_cyfc2civ_wpftmp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calendar")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+913b8cd3ca65c8672665e9e66e2e755f04b87de7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calendar")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calendar")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
