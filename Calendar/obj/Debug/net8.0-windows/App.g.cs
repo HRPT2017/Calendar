7be764934ab587212a1f4c1f75029b247c72c66a
Hugo Teixeira <hugo127c@gmail.com>
2024-02-27 19:50:58 +0000

    Final

--- Calendar/obj/Debug/net8.0-windows/App.g.cs
+++ Calendar/obj/Debug/net8.0-windows/App.g.cs
@@ -45,7 +45,7 @@ namespace Calendar {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.0.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.1.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace Calendar {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.0.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.1.0")]
         public static void Main() {
             Calendar.App app = new Calendar.App();
             app.InitializeComponent();
